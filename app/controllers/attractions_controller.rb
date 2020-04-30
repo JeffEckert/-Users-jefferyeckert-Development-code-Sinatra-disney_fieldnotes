32e4efba5fe1a9df5b3401194c69673404db263b
Jeff Eckert <bromodo@gmail.com>
2020-04-29 21:45:50 -0400

    added attractions controller and planned initial routes

---
+++ app/controllers/attractions_controller.rb
@@ -0,0 +1,30 @@
+class AttractionsController < ApplicationController
+
+    # CREATE
+         # New
+         # make get request to "/recipes/new"
+
+
+         # Create
+         # make a post request to "/attractions"
+
+    # READ
+         # Index
+         # make a get request to "/attractions"
+
+         # Show
+         # make a get request to "/attractions/:id"
+
+    # UPDATE
+        # Edit
+        # make get request to "/attractions/:id/edit"
+
+        # Update
+        # make a patch request to "/attractions/:id"
+
+    # DESTROY
+
+        # make a delete request to "/attractions/:id"
+
+
+end
