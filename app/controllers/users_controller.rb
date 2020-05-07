5ce4904f743ed9b04ff9c157ceaebd3cbd7e959d
Jeff Eckert <bromodo@gmail.com>
2020-05-07 17:26:08 -0400

    added signup erb view and some session scripting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,20 @@
 class UsersController < ApplicationController
     #users can sign up
 
+    get '/signup' do
+        erb :'users/signup'
+    end
+
     #users can delete account
+    post '/signup' do
+        user = User.new(params)
+        if user.username.empty? || user.password.empty?
+            @error = "Username or Password invalid Please try again."
+            erb :'users/signup'
+        else
+            user.save
+            session[:user_id] = user.id
+            redirect '/attractions'
+        end
+    end
 end
